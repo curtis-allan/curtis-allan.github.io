b64825d1f7a4f804cb111b70a21eb849d54f592e
Curtis Allan <curtis.allan@hotmail.com>
2018-09-18 10:18:50 +1000

    goof

--- server/controllers/admin_controller.js
+++ server/controllers/admin_controller.js
@@ -1,1 +1,52 @@
-module.exports = {};
+const Product = require("../models/product");
+const User = require("../models/user");
+
+module.exports = {
+  getAdminUsers(req, res) {
+    User.find({}).exec((err, users) => {
+      if (err) console.log("Find Admin Users Error---------------", err);
+
+      res.status(200).json({ users });
+    });
+  },
+
+  createProduct(req, res) {
+    const { name, description, price } = req.body;
+
+    let newProduct = new Product({
+      name,
+      description,
+      price
+    });
+    newProduct.save();
+
+    res.status(200).json({ product: newProduct });
+  },
+
+  updateProduct(req, res) {
+    const { id } = req.params;
+
+    Product.findById(id).exec((err, product) => {
+      if (err) console.log("Updated Product-----------------", err);
+      product.name = name;
+
+      product.description = description;
+
+      product.price = price;
+
+      product.save();
+
+      res.status(200).json({ product });
+    });
+  },
+
+  deleteProduct(req, res) {
+    const { id } = req.params;
+
+    Product.deleteOne({ _id: id }).exec((err, product) => {
+      if (err) console.log("Delete One Error-----------------", err);
+
+      res.status(200).json({ product });
+    });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use(bodyParser.json());
 // Storing user cookies
 app.use(
   session({
-    secret: process.en.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
 
     resave: false,
 
